dd06f9e01060cbfbad136bcb1e4ebc3a95542cae
peela trarun sai <tarunpeela1@gmail.com>
2024-09-24 14:21:06 +0530

    after updating

--- admission/models.py
+++ admission/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Admission(models.Model):
+    # id=models.IntegerField(primary_key=True)
     stu_name=models.CharField(max_length=100)
     stu_father=models.CharField(max_length=100)
     joindate=models.DateField()
-    stu_class=models.CharField(max_length=20,unique=True)
+    stu_class=models.CharField(max_length=100)
     fee=models.PositiveIntegerField()
 
 
